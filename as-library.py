74b478593580da2d90b55ca71c5e93aa2cf55bf3
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2023-04-12 09:35:52 -0500

    newchanges

--- as-library.py
+++ as-library.py
@@ -1,8 +1,8 @@
-from code import ExtremismDetector
+from code_1 import ExtremismDetector
 
 detector = ExtremismDetector()
 
-input_data = "I don't want to kill the president."
+input_data = "I want this president."
 
 extremism_score = detector.detect_extremism(input_data)
 print(extremism_score)
